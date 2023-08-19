03deb992cdd17de98764e6873b98499f624df870
Gustaf Ekström <gusek93@gmail.com>
2023-08-19 12:54:24 +0200

    Main content done. Added the cards and their components, aswell as styling for them

--- McDonalds-Clone-JavaScript/script.js
+++ McDonalds-Clone-JavaScript/script.js
@@ -10,7 +10,7 @@ function addChildElement(parentElement, element){
   parentElement.appendChild(element);
 }
   
-//Putting code to make each segment of the page into a function to increase readability
+//Putting code to make each segment of the page into a function to make it easier to read
 
 function designHeader (){
   
@@ -23,8 +23,8 @@ function designHeader (){
     {id: 'margin', value: '-10px'},
     {id: 'display', value: 'flex'},
     {id: 'position', value: 'relative'},
-    {id: 'boxShadow', value: 'rgba(0, 0, 0, 0.04) 0px 1px 8px 0px, rgba(0, 0, 0, 0.04) 0px 1px 5px 0px'},
-    {id: 'font-family', value: 'Speedee, Roboto, Arial'}
+    {id: 'boxShadow', value: 'rgba(0, 0, 0, 0.04) 0px 1px 8px 0px, rgba(0, 0, 0, 0.04) 0px 1px 5px 0px'}
+    
   ];
   applyStyles(styles, header);
  /* 
@@ -255,12 +255,196 @@ function designHeader (){
 }
 
 function designMainContent (){
-  const objectsToLisen = [];
+
+  const mainContent = document.createElement('div');
+  
+  //Main Headline
+  const h1 = document.createElement('h1');
+  h1.textContent = 'Meet Our Food Experts';
+  let styles = [
+    {id: 'font-weight', value: 'bold'},
+    {id: 'font-size', value: '54px'},
+    {id: 'padding', value: '0px'},
+    {id: 'margin', value: '0px'},
+    {id: 'margin-bottom', value: '20px'}
+  ];
+  applyStyles(styles, h1);
+  addChildElement(mainContent, h1);
+
+  //Image
+  const foodImage = document.createElement('img');
+  foodImage.src = 'Images/food-image.jpg';
+  addChildElement(mainContent, foodImage);
+
+  //Description
+  const desc = document.createElement('div');
+  const descText = document.createElement('span');
+  descText.textContent = 
+  "Our food experts are more than just pros at their craft." + 
+  "They are people who care about the food you eat and bring innovation into our kitchens";
+
+  styles = [
+    {id: 'font-size', value: '21px'},
+    {id: 'width', value: '1170px'},
+    {id: 'height', value: '100px'},
+    {id: 'padding', value: '0px 12px'},
+    {id: 'padding-top', value: '80px'},
+    {id: 'display', value: 'flex'},
+    {id: 'align-items', value: 'center'},
+    {id: 'line-height', value: '22px'}
+  ];
+  applyStyles(styles, desc);
+  addChildElement(desc, descText);
+  addChildElement(mainContent, desc);
+  
+  //bottom part of mainContent
+  const bottomContainer = document.createElement('div');
+  //first card
+  const card1  =document.createElement('div');
+  const card2  =document.createElement('div');
+  const card3  =document.createElement('div');
+  
+  const card1Text = document.createElement('span');
+  card1Text.textContent = 'Everyday, culinary masters are cooking up new tastes in our kitchens.';
+  const card2Text = document.createElement('span');
+  card2Text.textContent = 'Get to know our Registered Dietitian Nutritionists who add a different type of value to your meal.';
+  const card3Text = document.createElement('span');
+  card3Text.textContent = 'Meet some of the people who provide the ingredients for your favorite menu items.';
+  
+  [card1Text, card2Text, card3Text].forEach(cardText =>{
+    applyStyles([{id: 'font-size', value: '17px'},
+                {id: 'margin-top', value: '20px'}], cardText);
+  });
+
+  const chefImage = document.createElement('img');
+  chefImage.src = 'Images/chef.jpg';
+  const dietImage = document.createElement('img');
+  dietImage.src = 'Images/cynthia.jpg';
+  const supplierImage = document.createElement('img');
+  supplierImage.src = 'Images/supplierLetuce.jpg';
+
+  const headline1 = document.createElement('h2');
+  headline1.textContent = 'Our Chefs';
+  const headline2 = document.createElement('h2');
+  headline2.textContent = 'Our Chefs';
+  const headline3 = document.createElement('h2');
+  headline3.textContent = 'Our Chefs';
+
+  [headline1, headline2, headline3].forEach(headline =>{
+    applyStyles([{id: 'font-size', value: '32px'},
+                {id: 'font-weight', value: '700'},
+                {id: 'margin-top', value: '30px'},
+                {id: 'margin-bottom', value: '0px'}], headline)
+  });
+
+  
+  const button1 = document.createElement('button');
+  button1.textContent = 'Meet Our Chefs';
+  const button2 = document.createElement('button');
+  button2.textContent = 'Meet Our Dietitians';
+  const button3 = document.createElement('button');
+  button3.textContent = 'See Supplier Stories';
+
+  const buttons = [button1, button2, button3];
+  styles = [
+    {id: 'width', value: '140px'},
+    {id: 'height', value: '50px'},
+    {id: 'border', value: 'none'},
+    {id: 'border-radius', value: '3px'},
+    {id: 'background-color', value: 'rgb(255, 188, 13)'},
+    {id: 'margin-top', value: '40px'},
+    {id: 'font-size', value: '15px'}
+  ];
+  buttons.forEach(button =>{
+    applyStyles(styles, button);
+  });
+
+  applyStyles([{id: 'width', value: '170px'}], button2);
+  applyStyles([{id: 'width', value: '170px'}], button3);
+  //Styling elements
+  //styles for Bottom Container
+  styles = [
+    {id: 'height', value: '550px'},
+    {id: 'width', value: '1170px'},
+    {id: 'padding', value: '0px 12px'},
+    {id: 'padding-bottom', value: '70px'},
+    {id: 'padding-top', value: '50px'},
+    {id: 'display', value: 'flex'},
+    {id: 'flex-direction', value: 'row'},
+    {id: 'column-gap', value: '20px'},
+    {id: 'justify-content', value: 'center'},
+    {id: 'align-items', value: 'center'}
+  ];
+  applyStyles(styles, bottomContainer);
+
+  applyStyles([{id: 'width', value: '100%'}], chefImage);
+  applyStyles([{id: 'width', value: '100%'}], dietImage);
+  applyStyles([{id: 'width', value: '100%'}], supplierImage);
+  
+
+  
+  
+  //Styles for Card
+  const cards = [card1, card2, card3];
+  styles = [
+    {id: 'width', value: '380px'},
+    {id: 'height', value: '500px'},
+    {id: 'display', value: 'flex'},
+    {id: 'flex-direction', value: 'column'},
+    {id: 'text-align', value: 'left'}
+  ];
+
+  cards.forEach(card =>{
+    applyStyles(styles, card);
+  })
+  
+  
+
+  addChildElement(card1, chefImage);
+  addChildElement(card1, headline1);
+  addChildElement(card1, card1Text);
+  addChildElement(card1, button1);
+
+  addChildElement(card2, dietImage);
+  addChildElement(card2, headline2);
+  addChildElement(card2, card2Text);
+  addChildElement(card2, button2);
+
+  addChildElement(card3, supplierImage);
+  addChildElement(card3, headline3);
+  addChildElement(card3, card3Text);
+  addChildElement(card3, button3);
+  
+
+  addChildElement(bottomContainer, card1);
+  addChildElement(bottomContainer, card2);
+  addChildElement(bottomContainer, card3);
+  addChildElement(mainContent, bottomContainer);
+
+
+
+  //Styling mainContent
+  styles = [
+    {id: 'padding-top', value: '100px'},
+    {id: 'display', value: 'flex'},
+    {id: 'flex-direction', value: 'column'},
+    {id: 'align-items', value: 'center'}
+  ];
+  applyStyles(styles, mainContent);
+
+addChildElement(document.body, mainContent);
+
+  const objectsToLisen = [...buttons];
   return objectsToLisen;
 }
 
 //Defining array of objects to implement Eventlistener for hover functionality
+//The design functions should return array of objects that should have a hover function
 //Will add elements into this array from each section of the page.
+
+applyStyles([{id: 'padding-bottom', value: '50px'},
+            {id: 'padding', value: '0px'},
+            {id: 'color', value: 'rgb(41, 41, 41)'}],document.body);
  const hoverObjects = designHeader();
  
  hoverObjects.push(...designMainContent());
